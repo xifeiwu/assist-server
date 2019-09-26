0c9f1f06d145b4f0485e4edca57f03a96a3a5370
wuxifei <wuxifei@finupgroup.com>
2019-09-26 11:05:29 +0800

    extend axios in context

---
+++ app/extend/context.ts
@@ -0,0 +1,41 @@
+import Axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+
+interface ExtendConfig extends AxiosRequestConfig {
+    path?: string;
+    query?: string;
+}
+
+class AxiosHelper {
+    instance: AxiosInstance;
+    constructor(instanceConfig: AxiosRequestConfig) {
+        this.instance = Axios.create(Object.assign({
+            timeout: 15000,
+        }, instanceConfig));
+    }
+
+    async requestAxiosResponse(config: ExtendConfig) {
+        config.url = config.url ? config.url : config.path;
+        if (!config.url) {
+            throw new Error('参数不完整');
+        }
+        if (config.params && config.url) {
+            Object.keys(config.params).forEach(key => {
+                config.url = config.url ? config.url.replace('{' + key + '}', config.params[key]) : config.url;
+            });
+        }
+        config.params = config.query;
+        return await this.instance.request(config);
+    }
+
+    async requestData(config: ExtendConfig) {
+        const axiosResponse = await this.requestAxiosResponse(config);
+        const resData = axiosResponse.data;
+        return resData;
+    }
+}
+
+
+export default {
+    AxiosHelper,
+    axios: new AxiosHelper({}),
+};
