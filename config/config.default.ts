8a1b7d0d5acea84ed84aca39de443ffbdf98a114
wuxifei <wuxifei@finupgroup.com>
2020-01-13 14:56:03 +0800

    add app.secret, fix config.onerror.json

--- config/config.default.ts
+++ config/config.default.ts
@@ -5,7 +5,9 @@ export default (appInfo: EggAppInfo) => {
 
   // override config from framework / plugin
   // use for cookie sign key, should change to your own and keep security
-  config.key = appInfo.name + '_KcH0+LIP0HbuOvVwxHaZtBWSS';
+  config.keys = appInfo.name + '20200113';
+  // secret key for project
+  config.secret = appInfo.name + '_KcH0+LIP0HbuOvVwxHaZtBWSS';
 
   // add your egg config in here
   config.middleware = ['filter'];
@@ -32,13 +34,26 @@ export default (appInfo: EggAppInfo) => {
       if (!(err instanceof Error)) {
         err = new Error(`non-error thrown: ${err}`);
       }
-      // json hander
-      ctx.body = {
-        success: false,
-        t: Date.now(),
-        msg: err.message,
-      };
       ctx.status = err.status ? err.status : 200;
+      if (ctx.status == 200) {
+        /**
+          {
+            success: false
+            t: 1578881876483
+            msg: "userName is needed"
+          }
+         */
+        ctx.body = {
+          success: false,
+          t: Date.now(),
+          msg: err.message,
+        };
+      } else {
+        // [restful format ref](https://eggjs.org/zh-cn/tutorials/restful.html)
+        ctx.body = {
+          error: err.message
+        }
+      }
     },
   };
 
